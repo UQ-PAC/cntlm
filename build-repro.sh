142f443b465312a98b2a7730cbacaf09ee754588
Alistair Michael <alistair.michael@uq.edu.au>
2024-12-05 14:50:57 +1000

    use glibc

--- build-repro.sh
+++ build-repro.sh
@@ -2,17 +2,17 @@
 
 ROOT=$(git rev-parse --show-toplevel)
 CONT=$ROOT/docker-helper.sh
-export CC="$CONT aarch64-unknown-linux-musl-gcc"
+export CC="$CONT aarch64-unknown-linux-gnu-gcc"
 $CONT start
-# rm -rf build
-# cmake -B build . -DENABLE_DUK=1 
-# cmake --build build
-# mkdir -p package
-# cp build/cntlm-noduk package/cntlm-noduk-musl
-# cp build/cntlm-duk package/cntlm-duk-musl
+rm -rf build
+cmake -B build . -DENABLE_DUK=1 
+cmake --build build
+mkdir -p package
+cp build/cntlm-noduk package/cntlm-noduk
+cp build/cntlm-duk package/cntlm-duk
 
 cd package
-list='cntlm-noduk-musl cntlm-duk-musl'
+list='cntlm-noduk cntlm-duk'
 for NAME in $list; do
   $CONT aarch64-unknown-linux-musl-readelf $NAME -s -r -W > $NAME.relf
   $CONT bap -d adt:$NAME.adt -d bir:$NAME.bir $NAME 
@@ -20,7 +20,7 @@ for NAME in $list; do
   $CONT gtirb-semantics $NAME.gtirb $NAME.gts
 done
 
-#md5sum $(ls | grep -v '.md5') > checksum.md5
+md5sum $(ls | grep -v '.md5') > checksum.md5
 
 md5sum -c checksum.md5
 echo "Created from $(git remote get-url origin) $(git show-ref HEAD)" > readme.txt
